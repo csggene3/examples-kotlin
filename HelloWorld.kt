8bb3595504a138f8151096ebc6231b6e5d88ecb9
Wing Yung Chan <wy@users.noreply.github.com>
2017-08-31 21:58:45 +0100

    Updated with a return value

--- HelloWorld.kt
+++ HelloWorld.kt
@@ -1,7 +1,8 @@
 import AntShares.SmartContract.Framework.FunctionCode
 import AntShares.SmartContract.Framework.Services.AntShares.Storage
 class helloworld:FunctionCode() {
-    fun Main(args:Array<String>?) {
+    fun Main(args:Array<String>?) : ByteArray? {
             Storage.Put(Storage.getCurrentContext(), "Greeting to the World", "Hello World!")
+            return Storage.Get(Storage.getCurrentContext(),"Greeting to the World")
     }
 }
